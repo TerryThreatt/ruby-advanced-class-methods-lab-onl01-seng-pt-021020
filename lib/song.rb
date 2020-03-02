03fe9687c1e6cb193f57a373d2493ef61586868c
Terry Threatt <terry.threatt@gmail.com>
2020-03-02 22:32:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,6 +48,7 @@ class Song
   def self.new_from_filename(name)
     song = self.new 
     song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist = (name.split(" - ")[0])
     
   end 
     
