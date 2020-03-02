cc06fe30bbb2fa651789e6e3c838224e4e9e0a9a
Terry Threatt <terry.threatt@gmail.com>
2020-03-02 22:08:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,10 @@ class Song
     self.class.all << self
   end
   
-  def self.create
-    @@all << song = self.new  
+  def self.create(name)
+    song = self.new
+    song.name = name
+    @@all << song 
   end 
   
   def self.destroy_all 
