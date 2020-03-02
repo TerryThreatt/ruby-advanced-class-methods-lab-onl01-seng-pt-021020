76072381e2fad037d1716ab1a895d3beb1e3b13a
Terry Threatt <terry.threatt@gmail.com>
2020-03-02 22:30:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,9 @@ class Song
     @@all.sort_by {|n| n.name} 
   end 
   
-  def self.new_from_filename("")
+  def self.new_from_filename(name)
+    song = self.new 
+    song.name = (song.name{})
     
   end 
     
