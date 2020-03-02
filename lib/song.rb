95932287eb249072eafc5ba9197bd07b212c967d
Terry Threatt <terry.threatt@gmail.com>
2020-03-02 22:21:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,5 +36,10 @@ class Song
   def self.find_by_name(name)
     @@all.find {|n| n.name == name}
   end 
+  
+  def self.find_or_create_by_name(name)
+    if !@@all.find_by_name(name)
+      self.create_by_name(name)
+    end 
     
 end
